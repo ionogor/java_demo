fdee973d9bae63d5dae45777fa96913d7d37b924
Ion Ogor <ionogor87@gmail.com>
2021-04-22 19:10:03 +0300

    exercitiu la lectii

---
+++ Lesson3/exercitiu.java
@@ -0,0 +1,16 @@
+public class exercitiu{
+	public static void main(String[] args) {
+		int a=17;
+		int b=5;
+
+		int sum=a+b;
+		int dif=a-b;
+		int mult=a*b;
+		double doub = (double)a/b;
+
+		System.out.println(sum);
+		System.out.println(dif);
+		System.out.println(mult);
+		System.out.println(doub);
+	}
+}
